68d0414e58efc8191b610114c19c3d555be7200f
Jakub Czarski <jakczarski@gmail.com>
2020-02-04 15:55:26 +0100

    new file:   18.jpg 	new file:   18.psd 	new file:   img/Background.png 	new file:   img/Construction-Company-Website-Template-Free-PSD-D.png 	new file:   img/Creative-White-Business-Card-Free-PSD.png 	new file:   img/Layer3.png 	new file:   img/Nature-Tri-Fold-Brochure-Template-Free-PSD.png 	new file:   img/Professional-Resume-CV-Template-Free-PSD.png 	new file:   img/Rectangle.png 	new file:   img/Restaurant-Table-Tent-Template-Free-PSD.png 	new file:   img/Summer-Beach-Party-Flyer-Template-Free-PSD.png 	new file:   img/blog-img-1.png 	new file:   img/blog-img-2.png 	new file:   img/blog-img-3.png 	new file:   img/contact-us-bgc.png 	new file:   img/eclipse.png 	new file:   index.html 	new file:   js/jquery-3.4.1.min.js 	new file:   js/jquery.waypoints.min.js 	new file:   js/script.js 	new file:   style.css 	new file:   style.css.map 	new file:   style.scss

---
+++ js/script.js
@@ -0,0 +1,39 @@
+AOS.init();
+$("#designBar").waypoint(
+  function () {
+    $("#designBar").css("animation", "design 2s");
+  }, {
+    offset: "100%"
+  }
+);
+
+$("#photographyBar").waypoint(
+  function () {
+    $("#photographyBar").css("animation", "photography 2s");
+  }, {
+    offset: "100%"
+  }
+);
+
+$("#marketing-bar").waypoint(
+  function () {
+    $("#marketing-bar").css("animation", "marketing 2s");
+  }, {
+    offset: "100%"
+  }
+);
+
+$("#boarding-bar").waypoint(
+  function () {
+    $("#boarding-bar").css("animation", "branding 2s");
+  }, {
+    offset: "100%"
+  }
+);
+
+$(document).ready(function () {
+  $('.nav--mobile__toogle').click(function () {
+    $('nav').toggleClass('active--nav');
+    $('.fa-times::before').css('display', 'block');
+  })
+})
